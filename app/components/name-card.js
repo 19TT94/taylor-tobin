aa0505db81589e1f9d15786485d0905a85b80de6
Taylor Tobin <19tt94@gmail.com>
2017-06-16 19:16:27 -0700

    working name card

--- app/components/name-card.js
+++ app/components/name-card.js
@@ -5,10 +5,9 @@ export default Ember.Component.extend({
         Ember.run.scheduleOnce('afterRender', () => {
             illookinati({
                 target: document.querySelector('.card'),
-                max: 50, // optional max angle
-                targetPerspective: '800px', // optional perspective of the element
-                useDistance: false // optional, if true the angle of rotation will depend on
-                // cursor's distance from the target (frog gif uses it)
+                max: 20,
+                targetPerspective: '800px',
+                useDistance: false
             });
         });
     }
